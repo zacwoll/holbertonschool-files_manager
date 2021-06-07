0d07970e30af50ab972e92e7d2a431d37acee837
zacwoll <zacwoll@gmail.com>
2021-06-07 13:22:45 -0700

    Rewrote db, begun helper file

--- utils/db.js
+++ utils/db.js
@@ -1,35 +1,38 @@
-const { MongoClient } = require('mongodb');
+import { MongoClient } from 'mongodb';
 
+// Database client object
 class DBClient {
-    constructor() {
-        const host = process.env.DB_HOST || 'localhost';
-        const port = process.env.PORT || 27017;
-        const dbName = process.env.DB_DATABASE || 'files_manager';
-        this.client = new MongoClient(`mongodb://${host}:${port}`, { useUnifiedTopology: true });
-        this.client.connect((err) => {
-            if (err === null) {
-                this.db = this.client.db(dbName);
-            }
-            console.error(err);
-        });
-    }
+  constructor() {
+    this.host = process.env.DB_HOST || 'localhost';
+    this.port = process.env.DB_PORT || 27017;
+    this.dbName = process.env.DB_DATABASE || 'files_manager';
+    this.connected = false;
+    this.connectToClient();
+  }
 
-    isAlive() {
-        return this.client.isConnected();
-    }
+  // Set up connection and set data from DB
+  async connectToClient() {
+    MongoClient(`mongodb://${this.host}:${this.port}`, { useUnifiedTopology: true })
+      .connect()
+      .then(async (client) => {
+        this.client = client;
+        this.connected = true;
+        this.db = this.client.db(this.dbName);
+        this.files = await this.db.collection('files');
+        this.users = await this.db.collection('users');
+      })
+      .catch(console.error);
+  }
 
-    async nbUsers() {
-        const collection = this.db.collection('users');
-        const resArray = await collection.find({}).toArray();
-        return resArray.length;
-    }
+  // returns connection stats of the db
+  isAlive() { return this.connected; }
 
-    async nbFiles() {
-        const collection = this.db.collection('files');
-        const resArray = await collection.find({}).toArray();
-        return resArray.length;
-    }
+  // Returns the # of users
+  async nbUsers() { return this.users.countDocuments(); }
+
+  // Returns the # of files
+  async nbFiles() { return this.files.countDocuments(); }
 }
 
 const dbClient = new DBClient();
-module.exports = dbClient;
+export default dbClient;
---
+++ utils/helpers.js
@@ -0,0 +1,15 @@
+import redisClient from './redis';
+
+// checks authentication against verified information
+async function checkAuth(request) {
+    const token = request.headers['x-token'];
+    const key = `auth_${token}`;
+    const userId = await redisClient.get(key);
+    return userId || null;
+}
+
+// TODO: There should only be one user by userID, find a way to collapse result
+async function findUserById(userId) {
+    const userExistsArray = await dbClient.users.find(`ObjectId("${userId}")`).toArray();
+    return userExistsArray[0] || null;
+}
