73ed5bd7a315c84b9dc9245cfd230d4d1a228017
zacwoll <zacwoll@gmail.com>
2021-06-16 12:45:44 -0700

    sidestepped cyclic dep by fixing /users endpoint

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -8,10 +8,19 @@ class UsersController {
         const { email } = request.body;
         const { password } = request.body;
         const users = dbClient.db.collection('users');
-        const hashedPassword = await userInputValidation(response, email, password);
+        // Input Validation
+        if (!email) return response.status(400).json({ error: 'Missing email'});
+        if (!password) return response.status(400).json({error: 'Missing password' });
+        const usersWithEmail = await dbClient.users.find({ email }).toArray();
+        if (usersWithEmail.length > 0) return response.status(400).json({error: 'Already exists' });
+        // Validated User, Hashing pwd
+        const hashedPassword = await userInputValidation(password);
 
+        // Creating User and inserting into DB
         const result = await users.insertOne({ email, password: hashedPassword });
         const createdUser = { id: result.ops[0]._id, email: result.ops[0].email };
+
+        // Adding new user to userQueue
         userQueue.add({ userId: createdUser.id });
         response.statusCode = 201;
         return response.json(createdUser);
@@ -19,23 +28,7 @@ class UsersController {
 }
 
 // Potentially break up function into validation & password hashing
-async function userInputValidation(response, email, password) {
-    if (!email) {
-        response.statusCode = 400;
-        return response.json({ error: 'Missing email' });
-    }
-    if (!password) {
-        response.statusCode = 400;
-        return response.json({ error: 'Missing password' });
-    }
-
-    const users = dbClient.db.collection('users');
-    const existingUsers = await users.find({ email }).toArray();
-    if (existingUsers.length > 0) {
-        response.statusCode = 400;
-        return response.json({ error: 'Already exists'});
-    }
-
+async function userInputValidation(password) {
     const hashedPassword = crypto.createHash('SHA1').update(password).digest('hex');
     return hashedPassword;
 }
