dbdb111687a3b51dd2a900f549c341c6bb84f4c1
zacwoll <zacwoll@gmail.com>
2021-06-16 09:53:48 -0700

    Got the linter running

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,15 +1,16 @@
 import redisClient from './redis';
+import dbClient from './db';
 
 // checks authentication against verified information
 async function checkAuth(request) {
-    const token = request.headers['x-token'];
-    const key = `auth_${token}`;
-    const userId = await redisClient.get(key);
-    return userId || null;
+  const token = request.headers['x-token'];
+  const key = `auth_${token}`;
+  const userId = await redisClient.get(key);
+  return userId || null;
 }
 
 // TODO: There should only be one user by userID, find a way to collapse result
 async function findUserById(userId) {
-    const userExistsArray = await dbClient.users.find(`ObjectId("${userId}")`).toArray();
-    return userExistsArray[0] || null;
+  const userExistsArray = await dbClient.users.find(`ObjectId("${userId}")`).toArray();
+  return userExistsArray[0] || null;
 }
--- utils/redis.js
+++ utils/redis.js
@@ -1,36 +1,37 @@
 import redis from 'redis';
+
 const { promisify } = require('util');
 
 class RedisClient {
-    // Every client must create and connect to redis
-    constructor() {
-        this.client = redis.createClient();
-        this.client.on('error', (error) => console.error(`Redis client not connected to the server: ${error.message}`));
-    }
+  // Every client must create and connect to redis
+  constructor() {
+    this.client = redis.createClient();
+    this.client.on('error', (error) => console.error(`Redis client not connected to the server: ${error.message}`));
+  }
 
-    // Returns status of client
-    isAlive() {
-        return this.client.connected;
-    }
+  // Returns status of client
+  isAlive() {
+    return this.client.connected;
+  }
 
-    // Returns value at key from redis store
-    async get(key) {
-        const asyncGet = promisify(this.client.get).bind(this.client);
-        const value = await asyncGet(key).catch(console.error);
-        return value;
-    }
+  // Returns value at key from redis store
+  async get(key) {
+    const asyncGet = promisify(this.client.get).bind(this.client);
+    const value = await asyncGet(key).catch(console.error);
+    return value;
+  }
 
-    // Sets value into redis store
-    async set(key, value, duration) {
-        const asyncSet = promisify(this.client.set).bind(this.client);
-        await asyncSet(key, value, 'EX', duration).catch(console.error);
-    }
+  // Sets value into redis store
+  async set(key, value, duration) {
+    const asyncSet = promisify(this.client.set).bind(this.client);
+    await asyncSet(key, value, 'EX', duration).catch(console.error);
+  }
 
-    // Deletes value at redis store
-    async del(key) {
-        const asyncDel = promisify(this.client.del).bind(this.client);
-        await asyncDel(key).catch(console.error);
-    }
+  // Deletes value at redis store
+  async del(key) {
+    const asyncDel = promisify(this.client.del).bind(this.client);
+    await asyncDel(key).catch(console.error);
+  }
 }
 
 const redisClient = new RedisClient();
